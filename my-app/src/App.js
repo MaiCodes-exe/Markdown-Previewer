789f4f38d339f6456f3e32b27e68ba7e9f993e8c
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-16 21:50:13 +0100

    removing template code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,14 +8,6 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-      
-        </a>
         <Button variant="text">Text</Button>
       </header>
     </div>
