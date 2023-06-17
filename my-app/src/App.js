84cf0b7f1ded686883544bbb348405cdf32eaa6f
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-17 21:32:30 +0100

    adding markdown function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,11 @@ import {Button} from "@mui/material"
 
 
 function App() {
-
+  const [markdown, setMarkdown] = useState('');
+  const handleInputChange = (event) => {
+    setMarkdown(event.target.value);
+  };
+  
 }
 
 export default App;
