0e0dc5dd7b0eeb9d6821afe737f42f78eebb8b89
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-17 21:40:37 +0100

    testing new return statement

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,22 @@ function App() {
     return { __html: marked(markdown) };
   };
 
+  return (
+    <div className="App">
+      <div className="input-container">
+        <textarea
+          id="markdown-input"
+          value={markdown}
+          onChange={handleInputChange}
+        ></textarea>
+      </div>
+      <div
+        id="markdown-preview"
+        className="preview-container"
+        dangerouslySetInnerHTML={getMarkdownText()}
+      ></div>
+    </div>
+  );
 }
 
 export default App;
