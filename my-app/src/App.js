12616badb8d56eac619a191869843320dae9532e
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-21 21:47:13 +0100

    importing the markdwon d+code to the app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarkdownPreviewer from './MarkdownPreviewer';
+
+const App = () => {
+  return (
+    <div className="App">
+      <MarkdownPreviewer />
+    </div>
+  );
+};
+
+export default App;
