8924fafd829092e59760fe6b6df80f1f36547c9b
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-23 21:25:18 +0100

    testing in css file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './App.css'; 
+
 import  {MarkdownPreviewer} from './MarkdownPreviewer';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <MarkdownPreviewer />
     </div>
   );
